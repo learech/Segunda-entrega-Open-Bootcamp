165ab71b451fae227743ffe00efa8be4f941358a
Lea Rech <learech@hotmail.com>
2022-12-17 11:01:11 -0300

    subiendo

---
+++ ejercicio.js
@@ -0,0 +1,20 @@
+const nombre = "Leandro Rech";
+const edad = 38;
+const isDeveloper = true;
+const fechaDeNacimiento = new Date(1984, 7, 21);
+const libroFavorito = {
+    titulo : "1984",
+    autor : "George Orwell",
+    fecha : 1949,
+    url:'https://es.wikipedia.org/wiki/1984_(novela)'
+}
+
+const persona = [
+    nombre,
+    edad,
+    isDeveloper,
+    fechaDeNacimiento,
+    libroFavorito
+]
+
+console.log(persona);
